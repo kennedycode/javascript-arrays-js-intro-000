160781883cdbff6c37acbb8f28e539e44ee68767
Andrew Kennedy <akennedycode@gmail.com>
2020-07-18 20:08:09 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -23,5 +23,20 @@ function accessElementInArray(array, index) {
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
-  return array.shift()
+  array.shift()
+  return array
+}
+
+function removeElementFromBeginningOfArray(array) {
+  return array.slice(1)
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop()
+  return array
+}
+
+function removeElementFromEndOfArray(array)  {
+  array.slice(0, array.length - 1)
+  return array
 }
